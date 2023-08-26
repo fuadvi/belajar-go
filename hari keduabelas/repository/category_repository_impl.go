094aa29f0cd7d3707c45da63f7cb9db738851705
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-08-26 14:59:38 +0700

    implement repository

--- hari keduabelas/repository/category_repository_impl.go
+++ hari keduabelas/repository/category_repository_impl.go
@@ -1,35 +1,70 @@
 package repository
 
 import (
+	"belajar-resfull-api/helper"
 	"belajar-resfull-api/model/domain"
 	"context"
 	"database/sql"
+	"errors"
 )
 
 type CategoryRepositoryImpl struct {
 }
 
 func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category {
-	//TODO implement me
-	panic("implement me")
+	SQL := "insert into category(name) values (?)"
+	result, err := tx.ExecContext(ctx, SQL, category.Name)
+	helper.PanicIfError(err)
+
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
+	category.Id = int(id)
+
+	return category
 }
 
 func (repository *CategoryRepositoryImpl) Update(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category {
-	//TODO implement me
-	panic("implement me")
+	SQL := "update category set name = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Name)
+	helper.PanicIfError(err)
+
+	return category
 }
 
 func (repository *CategoryRepositoryImpl) Delete(ctx context.Context, tx sql.Tx, categoryId int) {
-	//TODO implement me
-	panic("implement me")
+	SQL := "delete category where id = ?"
+	_, err := tx.ExecContext(ctx, SQL, categoryId)
+	helper.PanicIfError(err)
 }
 
-func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx sql.Tx, categoryId int) domain.Category {
-	//TODO implement me
-	panic("implement me")
+func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx sql.Tx, categoryId int) (domain.Category, error) {
+	SQL := "select id, name from category where id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, categoryId)
+	helper.PanicIfError(err)
+
+	category := domain.Category{}
+	if rows.Next() {
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+	} else {
+		return category, errors.New("category not found")
+	}
+
+	return category, nil
 }
 
 func (repository *CategoryRepositoryImpl) FindAll(ctx context.Context, tx sql.Tx) []domain.Category {
-	//TODO implement me
-	panic("implement me")
+	SQL := "select id, name from category"
+	rows, err := tx.QueryContext(ctx, SQL)
+	helper.PanicIfError(err)
+
+	categories := []domain.Category{}
+	for rows.Next() {
+		category := domain.Category{}
+		err := rows.Scan(category.Id, category.Name)
+		helper.PanicIfError(err)
+	}
+
+	return categories
 }
