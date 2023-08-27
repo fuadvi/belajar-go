8705ab202b7627902f5f57a50fd87cf1633d0601
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-08-27 09:54:45 +0700

    validasi

--- hari keduabelas/model/web/category_create_request.go
+++ hari keduabelas/model/web/category_create_request.go
@@ -1,5 +1,5 @@
 package web
 
 type CategoryCreateRequest struct {
-	Name string
+	Name string `validated:"required, max=200,min=0"`
 }
--- hari keduabelas/model/web/category_update_request.go
+++ hari keduabelas/model/web/category_update_request.go
@@ -1,6 +1,6 @@
 package web
 
 type CategoryUpdateRequest struct {
-	Id   int
-	Name string
+	Id   int    `validated:"required"`
+	Name string `validated:"required, max=200,min=0"`
 }
