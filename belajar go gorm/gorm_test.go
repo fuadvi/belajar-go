31e0abdd191c753fad359871dc9db1833da3d219
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-12-11 20:27:28 +0700

    belajar gorm

---
+++ belajar go gorm/gorm_test.go
@@ -0,0 +1,24 @@
+package belajar_go_gorm
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"testing"
+)
+
+func OpenConnection() *gorm.DB {
+	dsn := "root:@tcp(127.0.0.1:3306)/belajar_golang_gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+var db = OpenConnection()
+
+func TestOpenConnection(t *testing.T) {
+	assert.NotNil(t, db)
+}
